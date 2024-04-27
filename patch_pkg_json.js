f16ea1d0dce8497687ec8034f4354adaa62cc552
elimerl <32183908+elimerl@users.noreply.github.com>
2024-04-27 14:13:43 -0700

    please work

--- patch_pkg_json.js
+++ patch_pkg_json.js
@@ -3,6 +3,7 @@ const packageFileContent = fs.readFileSync("./pkg/package.json", "utf-8");
 const packageJSON = JSON.parse(packageFileContent);
 packageJSON.type = "module";
 packageJSON.main = packageJSON.module;
+packageJSON.name = "@elimerl/fvd-rs";
 fs.writeFileSync(
     "./pkg/package.json",
     JSON.stringify(packageJSON, null, 4),
